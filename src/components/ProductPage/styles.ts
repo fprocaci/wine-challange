82bdd031a65a383c100225a6893e2017fd949e83
Fabricio Procaci <fprocaci@gmail.com>
2022-09-17 00:48:51 -0300

    Estilidando a página de produtos

--- src/components/ProductPage/styles.ts
+++ src/components/ProductPage/styles.ts
@@ -32,16 +32,21 @@ export const ContainerDesktop = styled.div`
     display: flex;
 
     .wineImg {
-      width: 50%;
-      height: 579px;
-      margin-left: 129px;
+      width: 40.563rem;
+      height: 580px;
+      /* margin-left: 129px; */
       display: flex;
       justify-content: center;
       align-items: center;
+      img {
+        width: 381px;
+        height: 579px;
+      }
     }
 
     .wineDescription {
-      width: 50%;
+      width: 462.22px;
+      margin-top: 10px;
       h1 {
         color: #111111;
         font-weight: 700;
@@ -49,7 +54,192 @@ export const ContainerDesktop = styled.div`
         line-height: 32px;
       }
       .wineResume {
+        width: 100%;
+        display: flex;
+        align-items: center;
         color: #555555;
+        svg {
+          margin-right: 8px;
+        }
+        .country {
+          font-family: 'Lato';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 24px;
+          margin-right: 8px;
+        }
+        .type {
+          font-family: 'Lato';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 24px;
+          margin-right: 8px;
+        }
+        .type2 {
+          font-family: 'Lato';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 24px;
+          margin-right: 8px;
+        }
+        .Weight {
+          font-family: 'Lato';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 24px;
+          margin-right: 8px;
+        }
+        .amount {
+          font-family: 'Lato';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 24px;
+          margin-right: 8px;
+        }
+      }
+    }
+    .winePrice {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      padding-top: 48px;
+      .sPrice {
+        width: 100%;
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 900;
+        font-size: 24px;
+        line-height: 32px;
+        color: #c81a78;
+      }
+      .nsPrice {
+        width: 100px;
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        text-transform: uppercase;
+        margin-top: 8px;
+
+        color: #888888;
+      }
+    }
+    .wineText {
+      width: 100%;
+      margin-top: 48px;
+      h3 {
+        font-family: 'Neo Sans Std';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 24px;
+        padding-bottom: 5px;
+
+        color: #111111;
+      }
+      p {
+        font-family: 'Saira';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 150%;
+        margin-top: 8px;
+
+        color: #666666;
+      }
+    }
+
+    .button {
+      width: 328px;
+      height: 56px;
+      background: #7ebc43;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 48px;
+
+      hr {
+        width: 2px;
+        height: 33px;
+        mix-blend-mode: normal;
+        opacity: 0.1;
+        border: 1px solid #ffffff;
+        opacity: 10%;
+      }
+
+      .count {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 42px;
+        .display {
+          width: 39px;
+          height: 42px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-right: 17px;
+          margin-left: 17px;
+
+          span {
+          }
+        }
+
+        .remove {
+          width: 30px;
+          height: 30px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 1px solid rgba(255, 255, 255, 0.4);
+          border-radius: 15px;
+
+          font-family: 'Open Sans SemiBold';
+          font-style: normal;
+          font-weight: 600;
+          font-size: 20px;
+          line-height: 27px;
+        }
+        .add {
+          width: 30px;
+          height: 30px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 1px solid rgba(255, 255, 255, 0.4);
+          border-radius: 15px;
+
+          font-family: 'Open Sans SemiBold';
+          font-style: normal;
+          font-weight: 600;
+          font-size: 20px;
+          line-height: 27px;
+        }
+      }
+      button {
+        width: 50%;
+        height: 100%;
+        background: #7ebc43;
+        border: none;
+
+        font-family: 'Lato';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        color: #ffffff;
       }
     }
   }
